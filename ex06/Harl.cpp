a43b18728fe449b9c1ba8c5eb1d0d7b6c2f213ce
ismet efe balık <efebalikofc@hotmail.com>
2023-11-30 18:45:28 +0300

    final but not finished

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -1,34 +1,46 @@
 #include "Harl.hpp"
 
 void Harl::debug( void ) {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info( void ) {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void Harl::warning( void ) {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I've been coming foryears whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void) {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
-#include "Harl.hpp"
-
 void Harl::complain(std::string level) {
-    a_functions funcs[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
     std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    int pos = -1;
-    for (int i = 0; i < 4; i++) {
-        if (level == levels[i]) {
-            pos = i;
+    int pos;
+    for(pos = 0; pos < 4; pos++)
+    {
+        if (level == levels[pos])
             break;
-        }
     }
-    if (pos != -1) {
-        (this->*funcs[pos])();
+    switch (pos)
+    {
+    case 0:
+        this->debug();
+    case 1:
+        this->info();
+    case 2:
+        this->warning();
+    case 4:
+        this->error();
+        break;
+    default:
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+        break;
     }
 }
