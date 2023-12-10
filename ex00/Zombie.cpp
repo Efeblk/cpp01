49694efe5b33c4e4f932454eef03887a79b492e0
ismet efe balik <ibalik@k1m07s09.42istanbul.com.tr>
2023-12-10 15:01:51 +0300

    uppercase

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() {
+}
+
+void Zombie::announce(void) {
+    if (name == "Foo") {
+        std::cout << name << " BraiiiiiiinnnzzzZ..." << std::endl;
+    } else {
+        std::cout << "<" << name << "> BraiiiiiiinnnzzzZ..." << std::endl;
+    }
+}
+
+void Zombie::setName(std::string name) {
+    this->name = name;
+}
