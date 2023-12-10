5cdebeb80a98b2c648c0b0891f47bdf4db6083a7
ismet efe balik <ibalik@k1m07s09.42istanbul.com.tr>
2023-12-10 15:11:30 +0300

    zombiehorde

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -4,7 +4,6 @@ Zombie *zombieHorde(int N, std::string name) {
     Zombie* zombieHorde = new Zombie[N];
     for (int i = 0; i < N; i++) {
         zombieHorde[i].setName(name);
-        zombieHorde[i].announce();
     }
     return zombieHorde;
 }
